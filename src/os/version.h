fe5828b9035da767c1be6b15eed47fdf3b250e55
Tomas Perez <tom@0x101.com>
2015-10-31 14:38:55 +0100

    Including parts of Sanos Operative system C library implementations.

---
+++ src/os/version.h
@@ -0,0 +1,8 @@
+#define VERSION_H
+#define OS_NAME      ""
+#define OS_COPYRIGHT ""
+#define OS_LEGAL     ""
+#define OS_MAJ_VERS  1
+#define OS_MIN_VERS  3
+#define OS_RELEASE   5
+#define OS_BUILD     0
